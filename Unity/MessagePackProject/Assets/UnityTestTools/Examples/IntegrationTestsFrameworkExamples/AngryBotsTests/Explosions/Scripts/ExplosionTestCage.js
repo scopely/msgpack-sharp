f800158884faa069f04645b20352e3c81703a48f
Clay Fowler <paul.clay.fowler@gmail.com>
2014-03-27 12:39:00 -0700

    Create naked branch for embedding (e.g. as submodule in other projects)

--- Unity/MessagePackProject/Assets/UnityTestTools/Examples/IntegrationTestsFrameworkExamples/AngryBotsTests/Explosions/Scripts/ExplosionTestCage.js
+++
@@ -1,8 +0,0 @@
-
-var explPrefab : GameObject;
-
-function OnTriggerEnter(other : Collider) {
-	if(other.GetComponent.<Collider>().tag == "Player") {
-		var go : GameObject = Instantiate(explPrefab, transform.position, transform.rotation);	
-	}	
-}
--- Unity/MessagePackProject/Assets/UnityTestTools/Examples/IntegrationTestsFrameworkExamples/AngryBotsTests/Scripts/Modules/EmitParticles.js
+++
@@ -1,6 +0,0 @@
-#pragma strict
-
-function OnSignal () 
-{
-	GetComponent.<ParticleEmitter>().emit = true;
-}
